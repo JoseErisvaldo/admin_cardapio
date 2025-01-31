d83676b757b0ac165844513e3d0e0c4b6bab2dc7
JoseErisvaldo <joseerisvaldopg@gmail.com>
2025-01-31 17:19:04 -0300

    Feat: add info in branch

--- src/components/Branch/index.jsx
+++ src/components/Branch/index.jsx
@@ -1,6 +1,28 @@
 import { Avatar } from "@material-tailwind/react";
+import { useEffect, useState } from "react";
+import supabase from "../../services/supabaseClient";
 
 export default function ViewBranch() {
+  const [branch, setBranch] = useState(null);
+
+  async function fetchBranch() {
+    try {
+      let { data: branches, error } = await supabase
+        .from("view_company_same_user")
+        .select()
+      if (error) throw error;
+      if (branches && branches.length > 0) {
+        setBranch(branches[0]);
+        console.log(branches[0]);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchBranch();
+  }, []);
   return (
     <div className="max-w-3xl mx-auto p-4">
       <div className="relative -z-0">
@@ -8,7 +30,7 @@ export default function ViewBranch() {
         <div className="h-52 w-full rounded-2xl overflow-hidden">
           <img
             className="object-cover w-full h-full"
-            src="https://marketplace.canva.com/EAD5DdSyaYI/1/0/1600w/canva-respira%C3%A7%C3%A3o-facebook-capa-EF9hf0DORFc.jpg"
+            src={branch?.cover_photo}
             alt="Profile Cover"
           />
         </div>
@@ -16,28 +38,28 @@ export default function ViewBranch() {
         {/* Avatar centralizado sobre a capa */}
         <div className="absolute left-1/2 transform -translate-x-1/2 -bottom-16">
           <Avatar
-            src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
+            src={branch?.profile_picture}
             alt="avatar"
             className="h-32 w-32 border-4 border-white shadow-lg rounded-full"
           />
         </div>
       </div>
       <div className="mt-8">
-        <h2 className="text-2xl font-bold mb-4">Nome da empresa</h2>
+        <h2 className="text-2xl font-bold mb-4">{branch?.name_branch}</h2>
         <p className="text-gray-600">
-          <span className="font-bold">Email:</span> email@email.com
+          <span className="font-bold">Email da empresa:</span> {branch?.email_branch}
         </p>
         <p className="text-gray-600">
-          <span className="font-bold">Telefone:</span> (99) 99999-9999
+          <span className="font-bold">CNPJ/CPF:</span> {branch?.cnpj_cpf}
         </p>
         <p className="text-gray-600">
-          <span className="font-bold">Empresa:</span> JS
+          <span className="font-bold">Telefone:</span> {branch?.phone}
         </p>
         <p className="text-gray-600">
-          <span className="font-bold">Total de funcionários:</span> 5
+          <span className="font-bold">Total de funcionários:</span> {branch?.total_users}
         </p>
         <p className="text-gray-600">
-          <span className="font-bold">Status:</span> Ativo
+          <span className="font-bold">Status:</span> {branch?.status}
         </p>
       </div>
     </div>
